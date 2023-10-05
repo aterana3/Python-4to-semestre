7634a5fcf4c738ca91a9194d70ab821bea3a60d3
Anthony Terán <108620290+aterana3@users.noreply.github.com>
2023-10-04 20:37:38 -0500

    Update Acta.py

--- coding/Acta.py
+++ coding/Acta.py
@@ -11,4 +11,5 @@ class Acta:
         for estudiante in curso.estudiantes:
             detActa = DetActa(estudiante)
             print(detActa.mostrarDatos())
-        
+
+    #Este es un mensaje de prueba
