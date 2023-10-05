c060bbbcd6f7bfa90f47a5ad0d38404b8d672ebb
roger <rogercornejo12@gmail.com>
2023-10-05 16:04:20 -0500

    HOLA

--- coding/Notas.py
+++ coding/Notas.py
@@ -1,6 +1,6 @@
 class Notas:
 
-    def __init__(self, n1 = 0, n2 = 0, ex1 = 0, n3 = 0, n4 = 0, ex2 = 0):
+    def __init__(self, n1=0, n2=0, ex1=0, n3=0, n4=0, ex2=0):
         self.n1 = n1
         self.n2 = n2
         self.ex1 = ex1
@@ -10,33 +10,39 @@ class Notas:
 
     @property
     def n1(self):
+        n1 = float(input("Ingrese nota n1: "))
         return self.__n1
-    
+
     @property
     def n2(self):
+        n2 = float(input("Ingrese nota n2: "))
         return self.__n2
 
     @property
     def ex1(self):
+        ex1 = float(input("Ingrese nota ex1: "))
         return self.__ex1
 
     @property
     def n3(self):
+        n3 = float(input("Ingrese nota n3: "))
         return self.__n3
-    
+
     @property
     def n4(self):
+        n4 = float(input("Ingrese nota n4: "))
         return self.__n4
-    
+
     @property
     def ex2(self):
+        ex2 = float(input("Ingrese nota ex2: "))
         return self.__ex2
 
     def promedio1(self):
-        return (self.__n1 + self.__n2 + self.__ex1)/3
+        return (self.__n1 + self.__n2 + self.__ex1) / 3
 
     def promedio2(self):
-        return (self.__n3 + self.__n4 + self.__ex2)/3
+        return (self.__n3 + self.__n4 + self.__ex2) / 3
 
     def nota_final(self):
         return (self.promedio1() + self.promedio2())
@@ -45,4 +51,9 @@ class Notas:
         return "Aprobado" if self.nota_final() >= 70 else "Reprobado"
 
     def mostrarDatos(self):
-        return "Nota 1: " + str(self.n1) + "\n" + "Nota 2: " + str(self.n2) + "\n" + "Examen 1: " + str(self.ex1) + "\n" + "Nota 3: " + str(self.n3) + "\n" + "Nota 4: " + str(self.n4) + "\n" + "Examen 2: " + str(self.ex2) + "\n" + "Promedio 1: " + str(self.promedio1()) + "\n" + "Promedio 2: " + str(self.promedio2()) + "\n" + "Nota final: " + str(self.nota_final()) + "\n" + "Estado: " + self.estado() + "\n"
+        return "Nota 1: " + str(self.n1) + "\n" + "Nota 2: " + str(self.n2) + "\n" + "Examen 1: " + str(
+            self.ex1) + "\n" + "Nota 3: " + str(self.n3) + "\n" + "Nota 4: " + str(self.n4) + "\n" + "Examen 2: " + str(
+            self.ex2) + "\n" + "Promedio 1: " + str(self.promedio1()) + "\n" + "Promedio 2: " + str(
+            self.promedio2()) + "\n" + "Nota final: " + str(
+            self.nota_final()) + "\n" + "Estado: " + self.estado() + "\n"
+
---
+++ coding/menu.py
@@ -0,0 +1,17 @@
+ 
+print("""
+1.Asignar notas
+2.Imprimir Acta de calificaiones
+""")
+a = int(input("Seleccione una opcion: "))
+
+while True:
+    if a == 1:
+        #Llamar a la funcion que asiganra notas
+        break;
+    elif a == 2:
+        #Llamar la funcion que imprimira el acta
+        break;
+    else:
+        print("Opcion no valida")    
+
