df0b48ca0fc631a04a77a5499528a847480b5457
Anthony Terán <108620290+aterana3@users.noreply.github.com>
2023-10-05 20:00:21 -0500

    Menu

--- Taller 1/menu.py
+++ Taller 1/menu.py
@@ -1,13 +1,14 @@
- 
+
 print("""
 1.Asignar notas
 2.Imprimir Acta de calificaiones
 """)
+
 a = int(input("Seleccione una opcion: "))
 
 while True:
     if a == 1:
-        #Llamar a la funcion que asiganra notas
+        
         break;
     elif a == 2:
         #Llamar la funcion que imprimira el acta
